fcafaa49d9c296e5ac3485c621def2003fb3f7b6
Elodie Aumaitre <60011569+EloloeA@users.noreply.github.com>
2020-07-28 17:38:54 +0200

    Add files via upload
    mise à jour version

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jul 15 10:20:18 2020
-
-@author: scriba
+Dashboard Carte identité du territoire - Juillet 2020
+by Elodie
 """
 
-
+# import des librairies
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from datetime import datetime as dt
-import dash_table
-import re
 from dash.dependencies import Input, Output
 import plotly.figure_factory as ff
 import os
-import pathlib
 
 import plotly.express as px
 import pandas as pd
 
 import outils
 
-# chargement des dataframes
-lien_df_portrait = "https://raw.githubusercontent.com/MeryemGrassi/Projet-CI-territoitre/master/data/df_PORTRAIT.csv"
+# chargement des dataframes df et df_hebergement
+lien_df_portrait = "https://raw.githubusercontent.com/EloloeA/carte_identite_territoriale/master/data/df_PORTRAIT.csv"
 df = pd.read_csv(lien_df_portrait, index_col = 0)
-# retirer les nan des PNR
+# retirer les valeurs nulles de la colonne PNR
 df['PNR'].fillna('', inplace = True)
+# Retirer les lettres capitales avec accents de la colonne COMMUNE car sinon ne se trie pas bien dans le dropdown
+df['COMMUNE'] = df['COMMUNE'].str.replace('É', 'E').str.replace('È', 'E')
 
-# échelle couleur pour les tables
-colorscale = [[0, '#014b86'],[.5, '#C1E2FA'],[1, '#ffffff']]
 
 dir = os.getcwd() + "\data"
-lien_df_resto = os.path.join(dir, "APIDAE_Final.csv")
-df_resto = pd.read_csv(lien_df_resto, index_col = 0)
+# lien = "C:\\Users\\scriba\\Drive_Go\\Wild_Code_School\\PROJET_4\\carte_identite_territoriale\\data\\APIDAE_Final.csv"
+lien_df_hebergement = os.path.join(dir, "APIDAE_Final.csv")
+df_hebergement = pd.read_csv(lien_df_hebergement, index_col = 0)
 
 
-# dictionnaire des départements pour le dropdown #TODO PB avec accents capital avec accent triés à la fin...
+# réglage échelle couleur pour les tables
+colorscale = [[0, '#014b86'],[.5, '#C1E2FA'],[1, '#ffffff']]
+
+# dictionnaire des départements pour le dropdown 
 dep = df[['CODE_INSEE', 'COMMUNE']]
 dep = dep.sort_values(by='COMMUNE')
 dep.columns = ['value', 'label']
@@ -47,13 +46,8 @@ dep = dep.to_dict(orient = 'records')
 #initialisation du dashboard
 app = dash.Dash(__name__)
 
-# colors = {
-#     'background': 'white',
-#     'text': '#0072bc'
-# }
 
-
-# carte
+# fonction carte
 def mapSud(df):
     geo = px.scatter_mapbox(df, lat='LATITUDE', lon='LONGITUDE', hover_name= 'COMMUNE',
                             size = 'POPULATION_2016', zoom=8, height=300, width = 300)
@@ -61,18 +55,15 @@ def mapSud(df):
     geo.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return geo
 
-
+# layout dash => construction des balises html et incorporation de graphiques (cartes et tables)
 app.layout = html.Div(id= 'main', children = [
                           html.Div(className = 'topHeader',
                                    children = [
-                                     html.H1(children = "Carte d'identité Territoriale - REGION SUD",
-                                     ),
-                             
-                                    html.H3(children = "Prototype d'application de l'utilisation des données OpenSource de la région SUD", 
-                                     ),
+                                    html.H1(children = "Carte d'identité Territoriale - REGION SUD"),
+                                    html.H3(children = "Prototype d'application de l'utilisation des données OpenSource de la région SUD"),
                                     html.Br(),
                                     html.P('Sélectionnez une ville :'),
-                             
+                                    # liste déroulante :
                                     dcc.Dropdown(
                                         id = 'cityChoice',
                                         options = dep,
@@ -96,8 +87,7 @@ app.layout = html.Div(id= 'main', children = [
                                                 ], style = {'margin' : '20px'}),
                                       html.Div(['Superficie (en km²) :',
                                                  html.Div(id = 'superficie')
-                                                ], style = {'margin' : '20px'})
-                                      ,
+                                                ], style = {'margin' : '20px'}),
                                       dcc.Graph(id = 'mapOfCity', className = 'mapLocalisation',
                                               figure = {})
                                               ]),
@@ -106,12 +96,13 @@ app.layout = html.Div(id= 'main', children = [
                             html.P(id = 'PNR'),
                             html.Br(),
                             html.Br(),
+                            
                             html.Div("Nombre d'hébergements (hôtels, campings, auberges)\
                                          pour cette commune : ", style = {'textAlign' : 'center'}),
                             html.Div(id = 'nbreLogement', style = {'textAlign' : 'center'}),
+                            
                             #table logement si existant ou < 5
-                            html.Div(dcc.Graph(id = 'hotel', figure = {})
-                                         ),
+                            html.Div(dcc.Graph(id = 'hotel', figure = {})),
                             
                             dcc.Graph(id = 'mapOfHotel', figure = {}),
                             
@@ -175,16 +166,16 @@ def update_map(input_value):
     fig2.update_layout(transition_duration=500)
     return fig2
 
-#MAJ carte des restaurants, hôtels et auberges
+#MAJ carte des hôtels, campings et auberges
 @app.callback(
     Output('mapOfHotel', 'figure'),
     [Input('cityChoice', 'value')])
-def update_mapResto(input_value):
-    df_sel = df_resto[df_resto['Code insee'] == input_value]
+def update_mapHebergement(input_value):
+    df_sel = df_hebergement[df_hebergement['Code insee'] == input_value]
     geo = px.scatter_mapbox(df_sel, lat='Latitude', lon='Longitude', hover_name= 'Nom',\
                         zoom = 10, height = 500, width = 800, title = "Hôtels, campings et Auberges",\
-                        color = df_sel.index) #, color_discrete_sequence = '#ff0000'
-    geo.update_layout(mapbox_style="carto-positron") #"open-street-map", "carto-positron", "carto-darkmatter", "stamen-terrain", "stamen-toner" or "stamen-watercolor"
+                        color = df_sel.index) 
+    geo.update_layout(mapbox_style="carto-positron")
     return geo
 
 # table avec liste hotels/camping/auberge
@@ -192,7 +183,7 @@ def update_mapResto(input_value):
     Output('hotel', 'figure'),
     [Input('cityChoice', 'value')])
 def liste_hotel(input_value):
-    df_sel = df_resto[df_resto['Code insee'] == input_value]
+    df_sel = df_hebergement[df_hebergement['Code insee'] == input_value]
     if df_sel.shape[0] == 0:
         nombre = 0
         commune = df[df['CODE_INSEE'] == input_value]['COMMUNE']
@@ -214,7 +205,7 @@ def liste_hotel(input_value):
     Output('nbreLogement', 'children'),
     [Input('cityChoice', 'value')])
 def nombre_logement(input_value):
-    df_sel = df_resto[df_resto['Code insee'] == input_value]
+    df_sel = df_hebergement[df_hebergement['Code insee'] == input_value]
     return df_sel.shape[0]
 
 # MAJ du PNR
@@ -226,9 +217,8 @@ def update_output_pnr(input_value):
     if resPNR != '':
         # pour un parc : nbre de villes
         nbre_ville_dans_le_PNR = df.loc[df['PNR'] == resPNR,'COMMUNE'].count()
-        # pour un parc : densité moyenne de population de la zone ?
+        # pour un parc : densité moyenne de population de la zone
         densiteMoy = df.loc[df['PNR'] == resPNR,'DENSITE_POP_2016'].mean()
-        # densiteMediane = df.loc[df['PNR'] == resPNR,'DENSITE_POP_2016'].median()
         densiteMoy = round(densiteMoy)
         return(f'La ville que vous avez choisie fait partie du {resPNR}. \
                Il y a {nbre_ville_dans_le_PNR} villes qui font parties du {resPNR}. \
@@ -245,8 +235,7 @@ def update_output_pnr(input_value):
                 Distance en km : {round(nbre_km)}")  
 
      
-
-
+# lancement de l'application:
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=True)
 
--- outils.py
+++ outils.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 17 11:39:45 2020
-
-@author: elo
+Outils Carte identité du territoire - Juillet 2020
+by Elodie
 """
 import pandas as pd
-#=> dans le main ?
+import haversine as hs # module de calcul de distance géographique
+
 
 # liens vers dataframe
-lien_df_portrait = "https://raw.githubusercontent.com/MeryemGrassi/Projet-CI-territoitre/master/data/df_PORTRAIT.csv"
-lien_distance_villes = "https://raw.githubusercontent.com/MeryemGrassi/Projet-CI-territoitre/master/data/distanceVilles.csv"
+lien_df_portrait = "https://raw.githubusercontent.com/EloloeA/carte_identite_territoriale/master/data/df_PORTRAIT.csv"
 
 
 # FONCTIONS
 
-# FAIRE une fonction init qui charge les df et renvoie le final;l'appeler depuis le main script
 def init():
     '''importe les différents df nécessaires'''
     df_PORTRAIT = pd.read_csv(lien_df_portrait, index_col = 0)
-    # TODO autres bases
     return df_PORTRAIT
 
 
@@ -33,7 +30,13 @@ def correspondanceInseeVille(insee):
     resVILLE = df_PORTRAIT.loc[df_PORTRAIT['CODE_INSEE'] == insee, 'COMMUNE'].values[0]
     return resVILLE
 
-# calcul d'un plus petit df de distances
+
+# calcul des distances des plus proches communes appartenant à un Parc Naturel Régional
+
+def calculDistance(loc1, loc2):
+    '''calcul de la distance entre 2 points géographiques avec loc1 et loc2 = tuples (long, lat)'''
+    return hs.haversine(loc1,loc2)
+
 def distanceCommunes(df, codeINSEE):
     '''création d'un df de distances avec code INSEE des communes'''
     df_PNR = df.loc[df["PNR"] != ""]
@@ -45,50 +48,9 @@ def distanceCommunes(df, codeINSEE):
     for j in liste_Com:
         deuxVille = df_PNR.loc[df_PNR['CODE_INSEE'] == j, 'COORD_GEO']
         dist = calculDistance(eval(premVille.values[0]), eval(deuxVille.values[0]))
-        df_temp.loc[j, 'distance'] = dist
-    #enregistrement en csv
-    # df_temp.to_csv(r'C:\Users\scriba\Drive_Go\Wild_Code_School\PROJET_4\distanceVillesv2.csv')   
+        df_temp.loc[j, 'distance'] = dist  
     id_ville = df_temp["distance"].idxmin()[0]
     return id_ville, df_temp.loc[id_ville, 'distance'].values[0]
-
-
-# def villeLaPlusProche(codeInsee, i = 1):
-#     '''fonction qui retourne le code INSEE de la ième plus proche ville de celle donnée'''
-#     distancesTab = pd.read_csv(lien_distance_villes, index_col = 0)
-
-#     # trier la ligne correspondantes à ce code Insee:
-#     distancesTab = distancesTab.sort_values(by=codeInsee, axis=1)
-#     # extraire ligne qui nous intéresse
-#     resDistance = distancesTab.loc[codeInsee]
-    
-#     #la première ligne = la même ville, distance de 0
-#     villeProche = resDistance.index[i]
-#     nbre_km = resDistance.iloc[i]
-#     return int(villeProche), nbre_km
-
-# # ATTENTION TEMPS d'EXECUTION ASSEZ LONG (fichier déjà crée sous = distanceVilles) DANS SRC ?
-import haversine as hs # calcul de distance géographique
-
-def calculDistance(loc1, loc2):
-    '''calcul de la distance entre 2 points géographiques avec loc1 et loc2 = tuples (long, lat)'''
-    return hs.haversine(loc1,loc2)
-
-# def distanceCommunes(df):
-#     '''création d'un df de distances avec code INSEE des communes'''
-#     liste_Com = list(df['CODE_INSEE'].unique())
-#     df_temp = pd.DataFrame(index = [liste_Com])
-#     for i in liste_Com:
-#         premVille = df.loc[df['CODE_INSEE'] == i, 'COORD_GEO']
-#         for j in liste_Com:
-#             deuxVille = df.loc[df['CODE_INSEE'] == j, 'COORD_GEO']
-#             dist = calculDistance(eval(premVille.values[0]), eval(deuxVille.values[0]))
-#             new_col = str(j)
-#             df_temp.loc[i, new_col] = dist
-#     #enregistrement en csv
-#     df_temp.to_csv(r'C:\Users\scriba\Drive_Go\Wild_Code_School\PROJET_4\distanceVilles.csv')   
-#     return df_temp
-
-#############
 
 
 
